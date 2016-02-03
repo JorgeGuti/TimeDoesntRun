29e3a32fbb8fbac08fcb8cc0e7241e0b4dbbedfb
inmaml <inmamangaslopez@gmail.com>
2016-02-03 18:52:54 +0100

    #25 crear script disparo
    en proceso de crearlo y probarlo

---
+++ Unity/Assets/Scripts/Personaje/Disparo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Disparo : MonoBehaviour {
+
+	public float velocidaddisparo= 1f;
+	Rigidbody2D rg;
+
+	// Use this for initialization
+	void Start () {
+		rg = GetComponent<Rigidbody2D>();
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rg.velocity = new Vector2(0f,velocidaddisparo);
+	
+	}
+}
