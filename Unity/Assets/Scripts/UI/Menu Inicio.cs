f09aab5624f48696750e003b3da4b151bcc4329a
jorge gutierrez <jorgegutierrezavila512@gmail.com>
2016-02-09 18:38:02 +0100

    #28

---
+++ Unity/Assets/Scripts/UI/Menu Inicio.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuInicio : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void NuevaPartida (){
+		Time.timeScale = 1f;		
+		Application.LoadLevel ("Nivel01");
+	}
+	public void Continuear (){
+		Time.timeScale = 1f;		
+		Application.LoadLevel ("Nivel04");
+	}
+	
+	public void Salir(){
+		Application.Quit ();
+	}
+
+}
