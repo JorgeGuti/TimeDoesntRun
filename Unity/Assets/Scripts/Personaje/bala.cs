fb8d6f96b3d7a0cd755c617970c86c96b5903608
inmaml <inmamangaslopez@gmail.com>
2016-02-09 17:02:52 +0100

    #Crear script de disparo
    Scripts

---
+++ Unity/Assets/Scripts/Personaje/bala.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bala : MonoBehaviour {
+	public float velocidad = 50f;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		rb.AddForce (transform.forward * 100);
+		Destroy (transform.gameObject, 3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity/Assets/Scripts/Personaje/mirar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mirar : MonoBehaviour {
+
+	public Transform target;
+	public GameObject bala;
+	public float tiempo_disparo = 3f;
+	private float prox_disparo = 0f;
+
+	void Start(){
+		prox_disparo = tiempo_disparo;
+	}
+
+	void Update (){
+
+		transform.LookAt (target);
+				if (Time.time >= prox_disparo) {
+					Instantiate(bala,transform.position, transform.rotation);
+					prox_disparo = Time.time + tiempo_disparo;
+	}
+}
+}
