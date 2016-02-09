c272e375f6717b30beaf26f593ee176cccb38314
carol555 <carolinaap4d@gmail.com>
2016-02-09 19:07:25 +0100

    #35 Crear Script Daño de Enemigos(modificado quita vidas)

---
+++ Unity/Assets/Scripts/Personaje/Herir.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+public class Herir : MonoBehaviour {
+
+	public int puntos = 5;
+	public float cadencia = 1f;
+	float proximo_hit = 0;
+	bool dentro_area = false;
+	GameControl gc;
+	// Use this for initialization
+	void Start () {
+		gc = GameObject.Find("GameControl").GetComponent<GameControl>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( (proximo_hit < Time.time) && dentro_area){
+			gc.resta_vida(puntos);
+			proximo_hit = Time.time + cadencia;
+		}
+	}
+
+	void OnTriggerEnter(Collider2D objeto){
+		if(objeto.tag == "Player"){
+			//Debug.Log("Entra Player");
+			dentro_area = true;
+		}
+	}
+
+	void OnTriggerExit2d(Collider2D objeto){
+		if(objeto.tag == "Player"){
+			//Debug.Log("Entra Player");
+			dentro_area = false;
+		}
+	}
+
+}
