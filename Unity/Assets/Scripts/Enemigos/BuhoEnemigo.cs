6a92df15ef0bff293fb7c056bce7b453e37d29b1
Clara <sariascl@gmail.com>
2016-02-23 19:30:54 +0100

    #49 buho animacion

---
+++ Unity/Assets/Scripts/Enemigos/BuhoEnemigo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuhoEnemigo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
