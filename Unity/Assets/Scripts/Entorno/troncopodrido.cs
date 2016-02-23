fb06ba76971f0f85711d879f202404a896c8a41c
carol555 <carolinaap4d@gmail.com>
2016-02-23 20:07:19 +0100

    #script tronco podrido

---
+++ Unity/Assets/Scripts/Entorno/troncopodrido.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class troncopodrido : MonoBehaviour {
+
+	public string nombre = "player";
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start (){
+		rb=GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D (Collider2D objeto){
+		string etiqueta = objeto.transform.tag;
+		Debug.Log (etiqueta);
+		if (etiqueta == nombre) {
+			rb.isKinematic = false;
+		}
+	}
+}
