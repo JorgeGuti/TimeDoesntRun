8e230ee749d7917a792db8ae314ae4e36aae1a6d
inmaml <inmamangaslopez@gmail.com>
2016-02-23 19:00:52 +0100

    #59
    Script pieza reloj, carga los niveles

---
+++ Unity/Assets/Scripts/UI/CargarNivel2.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CargarNivel2 : MonoBehaviour {
+
+	void OnCollisionEnter2D (Collision2D objeto){
+		if(objeto.transform.tag == "Player"){
+			Application.LoadLevel ("Nivel02");
+		}
+
+	}
+}
