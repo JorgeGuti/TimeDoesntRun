6b7f46d8a254c60ae117ec8882bb571f19146f6e
Clara <sariascl@gmail.com>
2016-02-24 17:24:33 +0100

    Clara [script buho] #56

--- Unity/Assets/Scripts/Enemigos/buhoEnemigo2.cs
+++ Unity/Assets/Scripts/Enemigos/buhoEnemigo2.cs
@@ -5,17 +5,34 @@ public class buhoEnemigo2 : MonoBehaviour {
 	
 		public Transform buho;
 		public Transform jugador;
-		public float speed = 1.0F;
+		public float velocidad = 1.0F;
 		private float startTime;
 		private float journeyLength;
+		public bool veo_a_edgar = false;
 		void Start() {
 			startTime = Time.time;
 			journeyLength = Vector3.Distance(buho.position, jugador.position);
 		}
 		void Update() {
-			float distCovered = (Time.time - startTime) * speed;
-			float fracJourney = distCovered / journeyLength;
-			transform.position = Vector3.Lerp(buho.position, jugador.position, fracJourney);
+			if(veo_a_edgar){
+				float distCovered = (Time.time - startTime) * velocidad;
+				float fracJourney = distCovered / journeyLength;
+				transform.position = Vector3.Lerp(buho.position, jugador.position, fracJourney);
+			}
+		}
+
+		void OnTriggerEnter2D(Collider2D objeto){
+			if(objeto.transform.tag == "Player"){
+				veo_a_edgar = true;
+			}
+
+		}
+
+		void OnTriggerExit2D(Collider2D objeto){
+		if(objeto.transform.tag == "Player"){
+			veo_a_edgar = false;
+		}
+		
 		}
 	}
 
