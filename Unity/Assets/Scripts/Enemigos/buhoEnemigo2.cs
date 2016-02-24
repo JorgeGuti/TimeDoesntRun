c6dcfe71314fce0a96e60082ae1abbbd88af34e8
Clara <sariascl@gmail.com>
2016-02-24 17:10:48 +0100

    Clara [anim,script buho] #49 #52

---
+++ Unity/Assets/Scripts/Enemigos/buhoEnemigo2.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buhoEnemigo2 : MonoBehaviour {
+	
+		public Transform buho;
+		public Transform jugador;
+		public float speed = 1.0F;
+		private float startTime;
+		private float journeyLength;
+		void Start() {
+			startTime = Time.time;
+			journeyLength = Vector3.Distance(buho.position, jugador.position);
+		}
+		void Update() {
+			float distCovered = (Time.time - startTime) * speed;
+			float fracJourney = distCovered / journeyLength;
+			transform.position = Vector3.Lerp(buho.position, jugador.position, fracJourney);
+		}
+	}
+
