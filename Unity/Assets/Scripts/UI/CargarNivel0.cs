312c437ec06651de8987838066c44fcc74fa4650
jorge gutierrez <jorgegutierrezavila512@gmail.com>
2016-03-02 17:48:22 +0100

    arreglado menu ingame, ya se puede pulsar

---
+++ Unity/Assets/Scripts/UI/CargarNivel0.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CargarNivel0 : MonoBehaviour {
+	
+	void OnCollisionEnter2D (Collision2D objeto){
+		if(objeto.transform.tag == "Player"){
+			Application.LoadLevel ("Nivel0");
+		}
+		
+	}
+}
